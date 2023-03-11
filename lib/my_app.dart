e288e0442e5e453ec9a52c60c326b7f62cb2b9f6
cindi_star <cindi_nishi@hotmail.com>
2023-03-10 23:55:13 -0300

    adding error on purpose to test CI

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       title: 'finance_app',
       theme: ThemeData(
@@ -33,6 +34,7 @@ class MyApp extends StatelessWidget {
         Locale('pt'),
       ],
       initialRoute: '/splash',
+
       routes: {
         '/login': (context) => const LoginPage(),
         '/forgot': (context) => const ForgotPasswordPage(),
